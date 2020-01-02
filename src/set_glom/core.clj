6b1b01bcd8efb1bc60c51c42d989afa0e3873533
mes32 <stockman.mike@gmail.com>
2020-01-02 17:00:04 -0600

    Parse entered lines and convert to hash-set

--- src/set_glom/core.clj
+++ src/set_glom/core.clj
@@ -8,11 +8,23 @@
     (flush)
     (read-line)))
 
+(defn parse-set
+  "Parse a space delimited string of items and return those items in a hash-set"
+  [string]
+  (set (clojure.string/split string #"\s+")))
+
+(defn glom-sets
+  "For now just prints the new set to add, and returns the initial vector of sets"
+  [sets-vec new-set]
+  (do
+    (println new-set)
+    sets-vec))
+
 (defn -main
   "Read in sets of items from the command line. Combine the sets as needed."
   []
-  (loop [line nil]  
-    (when (not= line "quit")
-      (def new-line (get-line))
-      (println new-line)    
-      (recur new-line))))
+  (loop [sets-vec []]
+    (def line (get-line))
+    (if (= line "quit")
+      sets-vec
+      (recur (glom-sets sets-vec (parse-set line))))))
