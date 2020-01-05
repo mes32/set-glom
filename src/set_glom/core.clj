12ba5360cd73ee9d4e60573fc41dee3fea463dbd
mes32 <stockman.mike@gmail.com>
2020-01-05 13:49:53 -0600

    Add a reduce and filter to main iteration. Reduce to combine sets as needed and filter remove existing copies of combined sets

--- src/set_glom/core.clj
+++ src/set_glom/core.clj
@@ -16,16 +16,37 @@
   [string]
   (set (string/split (string/trim string) #"\s+")))
 
+(defn display-sets
+  "Expects a list of sets and prints each set with nice formatting"
+  [sets]
+  ; (map
+  ;   (fn [item] (println (str "  {" (clojure.string/join ", " item) "}")))
+  ;   sets))
+  (println sets))
+
 (defn glom-sets
-  "For now just prints the new set to add, and returns the initial vector of sets"
+  "Given two sets 'target' and 'additional'. Returns the union if they have intersecting items. Otherwise returns the target set unchanged."
+  [target additional]
+  (if (empty? (set/intersection target additional))
+    target
+    (set/union target additional)))
+
+(defn iterate-main
+  "Generate a new iteration of the list of sets. Combine sets as needed using 'glom-sets' and combine with any unaffected sets."
   [current-sets new-set]
-  (set/union current-sets new-set))
+  (do
+    (def sets
+      (conj
+        (filter #(empty? (set/intersection new-set %)) current-sets)
+        (reduce (fn [target-set additional-set] (glom-sets target-set additional-set)) new-set current-sets)))
+    (display-sets sets)
+    sets))
 
 (defn -main
   "Read in sets of items from the command line. Combine the sets as needed."
   []
-  (loop [sets #{}]
+  (loop [sets '()]
     (def line (get-line))
     (if (= line "quit")
       sets
-      (recur (glom-sets sets (parse-set line))))))
+      (recur (iterate-main sets (parse-set line))))))
